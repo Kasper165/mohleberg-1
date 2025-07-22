93efbcbd533d6a6bb8d414b56be44077bbdf84c4
Kasper165 <stackstoring@gmail.com>
2025-07-21 22:10:21 -0400

    Delete mohlenberg-website-2 directory

--- mohlenberg-website-2/app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function HomePage() {
-  return (
-    <main className="p-8">
-      <h1 className="text-4xl font-bold">Velkommen til Møhlenberg</h1>
-      <p className="mt-4 text-lg">Professionel snedker og montage.</p>
-    </main>
-  )
-}
